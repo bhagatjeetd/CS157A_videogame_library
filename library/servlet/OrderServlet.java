3ab1c4b1d45125608fbbc302e7da6ab793042c22
alexnavarr05 <108955027+alexnavarr05@users.noreply.github.com>
2025-05-09 17:10:35 -0700

    Update OrderServlet.java
    proper date conversion

--- library/servlet/OrderServlet.java
+++ library/servlet/OrderServlet.java
@@ -62,15 +62,19 @@ public class OrderServlet extends HttpServlet {
                 int gameId = Integer.parseInt(request.getParameter("gameId"));
                 int duration = Integer.parseInt(request.getParameter("duration"));
 
-                Date today = new Date();
-                Date due = new Date(today.getTime() + (duration * 86400000L)); // ms/day
-                Game game = gameDAO.getGameById(gameId);
+                java.util.Date utilToday = new java.util.Date();
+                java.util.Date utilDue = new java.util.Date(utilToday.getTime() + (duration * 86400000L));
 
+                java.sql.Date today = new java.sql.Date(utilToday.getTime());
+                java.sql.Date due = new java.sql.Date(utilDue.getTime());
+
+                Game game = gameDAO.getGameById(gameId);
                 int orderId = orderDAO.createOrder(customer.getId(), today, due, game.getBaseRentalPrice());
                 itemDAO.addItem(orderId, gameId, game.getBaseRentalPrice());
 
                 response.sendRedirect("order");
-            }
+          }
+
         } catch (Exception e) {
             throw new ServletException(e);
         }
